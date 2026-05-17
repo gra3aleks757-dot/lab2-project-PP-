5e20e711ec761752f90605de32d72e48f8834593
Grachev Aleksandr <gra3aleks757@example.com>
2026-05-17 16:03:25 +0300

    Add multiply function

--- main.py
+++ main.py
@@ -4,5 +4,9 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
+def multiply(a, b):
+    return a * b
+
 print("5 + 3 =", add(5, 3))
 print("10 - 4 =", subtract(10, 4))
+print("6 * 7 =", multiply(6, 7))
